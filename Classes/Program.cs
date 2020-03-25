2695e53afabaf11a153ca403b6ac8fae9989c3ca
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 20:56:50 +0300

    Initial commit

---
+++ Classes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    /* 
+        Erişim_Belirteci class Classİsmi
+        {
+           Kodlar
+        }
+         
+         
+         */ 
+
+    //public class ToplamBul
+    //{
+    //    public int a;
+    //    public int b;
+    //    public int t;
+
+    //    public int Topla(int a, int b)
+    //    {
+    //        return t = a + b;
+    //    }
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToplamBul TB = new ToplamBul();
+
+            Console.WriteLine("A Değerini Giriniz:");
+            TB.a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("B Değerini Giriniz:");
+            TB.b = Convert.ToInt32(Console.ReadLine());
+
+            ToplamBul TC = new ToplamBul();
+
+            Console.WriteLine(TB.TB(TB.a, TB.b));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes/ToplamBul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class ToplamBul
+    {
+        public int a;
+        public int b;
+        
+
+        public int TB(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
